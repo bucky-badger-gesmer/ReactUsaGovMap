251e98bc8c83a86917c4ccfcc8be993f3aa64468
Aaron Gesmer <ajgesmer@comcast.net>
2020-11-12 22:27:23 -0500

    added color wheel, font awesome

--- src/Map.js
+++ src/Map.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import USAMap from 'react-usa-map';
+import { SketchPicker } from 'react-color';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 import { states } from './Helper';
-
 require('./Map.css');
 
 export default class Map extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            mapColor: 'gold'
+            mapColor: '#FFD701',
         };
     }
-
 
     mapHandler = (event) => {
         let foo = states[event.target.dataset.name];
@@ -19,13 +20,19 @@ export default class Map extends React.Component {
         alert(event.target.dataset.name, foo);
     };
 
+    handleChangeComplete(color) {
+        this.setState({ mapColor: color.hex });
+    }
+
     render() {
+        console.log('fuck me', this.state)
         return (
             <div>
-                <h1>USA!</h1>
+                <h1>USA!  <FontAwesomeIcon icon={faCog} /></h1>
                 <div className="usa-map">
                     <USAMap defaultFill={this.state.mapColor} onClick={this.mapHandler} />
                 </div>
+                <SketchPicker color={this.state.mapColor} onChangeComplete={this.handleChangeComplete.bind(this)} />
             </div>
         );
     }
