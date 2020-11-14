8ed2c35690ea6a1db3efbe2c7e30b33d9becb91f
Aaron Gesmer <ajgesmer@comcast.net>
2020-11-14 18:31:53 -0500

    converted to hooks!

--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import USAMap from 'react-usa-map';
 import { SketchPicker } from 'react-color';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,43 +7,32 @@ import { Popup } from 'semantic-ui-react';
 import { states } from './Helper';
 require('./Map.css');
 
-export default class Map extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mapColor: '#FFD701',
-        };
-    }
+export default function Map() {
+    const [mapColor, setMapColor] = useState('#FFD701');
 
-    mapHandler = (event) => {
+    const mapHandler = event => {
         let foo = states[event.target.dataset.name];
         console.log('foo', foo)
         alert(event.target.dataset.name, foo);
     };
 
-    handleChangeComplete(color) {
-        this.setState({ mapColor: color.hex });
+    const handleChangeComplete = color => {
+        setMapColor(color.hex);
     }
 
-    render() {
-        let icon = <FontAwesomeIcon style={{ float: 'right', marginRight: '50px' }} icon={faCog} />;
-        let colorPicker = <SketchPicker color={this.state.mapColor} onChangeComplete={this.handleChangeComplete.bind(this)} />;
-        let popup = <Popup
-                        trigger={icon}
-                        content={colorPicker}
-                        on='click' />;
+    const icon = <FontAwesomeIcon style={{ float: 'right', marginRight: '50px' }} icon={faCog} />;
+    const colorPicker = <SketchPicker color={mapColor} onChangeComplete={handleChangeComplete} />;
+    const popup = <Popup
+                    trigger={icon}
+                    content={colorPicker}
+                    on='click' />;
 
-        return (
-            <div>
-                <h1>USA!{popup}</h1>
-                <div className="usa-map">
-                    <USAMap defaultFill={this.state.mapColor} onClick={this.mapHandler} />
-                </div>
+    return (
+        <div>
+            <h1>USA!{popup}</h1>
+            <div className="usa-map">
+                <USAMap defaultFill={mapColor} onClick={mapHandler} />
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-
-
-
