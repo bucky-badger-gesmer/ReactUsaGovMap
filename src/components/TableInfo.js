c28e6337f72138ecf786efde0ae43746a37ac007
Aaron Gesmer <ajgesmer@comcast.net>
2021-01-03 10:20:23 -0500

    added datagrid

--- src/components/TableInfo.js
+++ src/components/TableInfo.js
@@ -1,65 +1,100 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { getMemberAge, getMemberParty, states } from '../Helper';
 
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { DataGrid, } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux';
 
-const useStyles = makeStyles({
-    table: {
-      minWidth: 650,
-    },
-  });
-  
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-
+require('../css/TableInfo.css');
 
 export default function TableInfo(props) {
-    const mapColor2 = useSelector((state) => state.color);
-    const dispatch = useDispatch();
-    console.log('mapColor2', mapColor2, props);
+    const color = useSelector((state) => state.color);
+    const useStyles = makeStyles({
+        root: {
+            '& .firstName': {
+                backgroundColor: color,
+            },
+            '& .lastName': {
+                backgroundColor: color
+            },
+            '& .state': {
+                backgroundColor: color
+            },
+            '& .age': {
+                backgroundColor: color
+            },
+            '& .seniority': {
+                backgroundColor: color
+            },
+            '& .title': {
+                backgroundColor: color
+            },
+            '& .party': {
+                backgroundColor: color
+            },
+            '& .totalVotes': {
+                backgroundColor: color
+            },
+            '& .missedVotes': {
+                backgroundColor: color
+            },
+            '& .totalPresent': {
+                backgroundColor: color
+            }
+        },
+    });
 
     const classes = useStyles();
+
+    const columns = [
+        { field: 'firstName', headerClassName: 'firstName', headerAlign: 'center', headerName: 'First Name', width: 150 },
+        { field: 'lastName', headerClassName: 'lastName', headerAlign: 'center', headerName: 'Last Name', width: 150 },
+        { field: 'state', headerClassName: 'state', headerAlign: 'center', headerName: 'State', width: 180 },
+        { field: 'age', headerClassName: 'age', headerAlign: 'center', headerName: 'Age', type: 'number', width: 110 },
+        { field: 'seniority', headerClassName: 'seniority', headerAlign: 'center', headerName: 'Seniority', type: 'number', width: 110 },
+        { field: 'title', headerClassName: 'title', headerAlign: 'center', headerName: 'Title', width: 160 },
+        { field: 'party', headerClassName: 'party', headerAlign: 'center', headerName: 'Party', width: 120 },
+        { field: 'totalVotes', headerClassName: 'totalVotes', headerAlign: 'center', headerName: 'Total Votes', type: 'number', width: 130 },
+        { field: 'missedVotes', headerClassName: 'missedVotes', headerAlign: 'center', headerName: 'Missed Votes', type: 'number', width: 130 },
+        { field: 'totalPresent', headerClassName: 'totalPresent', headerAlign: 'center', headerName: 'Total Present', type: 'number', width: 130 }
+    ];
+
+    let members = null;
+    if (props.senators && props.representatives) {
+        members = props.senators.concat(props.representatives).sort((a, b) => (a.last_name > b.last_name) ? 1 : -1)
+    }
+
+    let rows = [];
+    if (members) {
+        rows = members.map(member => {    
+            return {
+                id: member.id,
+                firstName: member.first_name,
+                lastName: member.last_name,
+                state: states[member.state],
+                age: getMemberAge(member.date_of_birth),
+                seniority: member.seniority,
+                title: member.title,
+                party: getMemberParty(member.party),
+                totalVotes: member.total_votes,
+                missedVotes: member.missed_votes,
+                totalPresent: member.total_present,
+                member: member
+            };
+        });
+
+    
+        console.log('rows', rows)
+    }
+
     return (
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-            <TableHead>
-                <TableRow>
-                <TableCell><strong>Dessert (100g serving)</strong></TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {rows.map((row) => (
-                <TableRow key={row.name}>
-                    <TableCell component="th" scope="row">
-                    {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                </TableRow>
-                ))}
-            </TableBody>
-            </Table>
-        </TableContainer>
+        <div style={{ height: 400, width: '95%', margin: 'auto', marginTop: '50px' }} className={classes.root}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={10}
+                rowsPerPageOptions={[10, 20, 50, 100]}
+                onRowClick={(RowParams) => console.log('RowParams', RowParams)}
+                disableSelectionOnClick={true} />
+        </div>
     );
 }
